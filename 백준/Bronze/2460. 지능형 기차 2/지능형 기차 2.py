cd7bd0a6f2db6d9c2e3b0b7f528f77f392904f62
Chae Eun Kim <78145403+kimchaeeun3447@users.noreply.github.com>
2023-08-07 22:10:21 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -1,14 +1,7 @@
-# [누적된 탄 사람] = [누적된 탄 사람] - 현재역의 내린 사람 수 + 현재역에 탄 사람 수
-total = 0 #누적
-max_total = 0
+s = [0]
 
-for i in range(1, 11):
+for i in range(10):
     a, b = map(int, input().split())
+    s.append(s[i] - a + b)
 
-    now = total - a + b
-    
-    max_total = now if now > max_total else max_total
-    total = now
-
-
-print(max_total)
+print(max(s))
