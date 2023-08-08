98f393426ce0b8248360457b5cd61008a61ba471
Chae Eun Kim <78145403+kimchaeeun3447@users.noreply.github.com>
2023-08-08 21:35:58 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,8 +1,16 @@
-a, b = map(int, input().split())
+#1) 최대공약수 - 유클리드 호제법
+# x / y를 계속하면서 나머지가 0일 때의 y값이 최대 공약수
+# x에는 y값을, y에는 나머지를 넣으며 계속 반복
 
-def gcd(a, b):
-    if b == 0:
-        return a
-    return gcd(b, a % b)
-    
-print(gcd(a, b), a*b//gcd(a, b))
+def gcd(x, y):
+    while(y):
+        x, y = y, x % y
+    return x
+
+#2) 최소공배수 - x*y // 최대공약수
+def lcd(x, y):
+    return (x * y) // gcd(x, y)
+
+x, y = map(int, input().split())
+print(gcd(x, y))
+print(lcd(x, y))
