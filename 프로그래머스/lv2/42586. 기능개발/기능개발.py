4bc71262de6b0eb2a05a57fbd47a46ca1e73f258
Chae Eun Kim <78145403+kimchaeeun3447@users.noreply.github.com>
2023-08-29 00:52:03 +0900

    [level 2] Title: 기능개발, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -2,18 +2,27 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    _progresses = [] #완성하는데 걸리는 일수
-    front = 0
     
-    #두 리스트 한 번에 루프 돌리기
-    for a, b in zip(progresses, speeds):
-        _progresses.append(math.ceil((100 - a) / b))
+    #각 작업이 100%되는 데 걸리는 시간 가공
+    # 올림처리
+    _progresses = []
+    for p, s in zip(progresses, speeds):
+        _progresses.append(math.ceil((100 - p) / s))
+
+    front = 0 #앞쪽 비교대상
 
     for i in range(len(_progresses)):
-        if _progresses[i] > _progresses[front]:  
+        # front보다 커질 경우, front ~ i-1번째까지 삽입 = i-front 개
+        if _progresses[front] < _progresses[i]:
             answer.append(i - front)
-            front = i
-            
-    answer.append(len(_progresses) - front)  
+            front = i #i번째부터 다시 시작        
 
+    answer.append(len(_progresses)- front)
     return answer
+
+print(solution([95, 90, 99, 99, 80, 99], [1,1,1,1,1,1]))
+
+#[5, 10, 1, 1, 20, 1]일 걸림
+# 5 < 10 -> 5 (1개) -> [5, -4, -4, 15, -4]
+# 5 > -4  & 5 > -4 / 5 < 15 -> 5, -4, -4 (3개) -> [10, -9]
+# 10 > -9-> 10, -9(2개)
