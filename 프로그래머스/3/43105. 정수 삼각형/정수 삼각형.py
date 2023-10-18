90cacfc0e3a723ac8bd0b6c027dd6b400daf4873
Chae Eun Kim <78145403+kimchaeeun3447@users.noreply.github.com>
2023-10-19 02:54:46 +0900

    [level 3] Title: 정수 삼각형, Time: 45.83 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -3,18 +3,22 @@
 -> dp[a][b] 에는 dp[a-1][b] 또는 dp[a-1][b-1] 중에 더 큰 값을 더함
 '''
 
-def solution(triangle):
-    answer = 0
-    row, col = len(triangle) + 1, len(triangle[-1]) + 1
-    
-    dp = [[0] * col for _ in range(row)]
-    
+def solution(triangle):        
     for r_idx, r in enumerate(triangle):
+        r_length = len(r)
+        
         for c_idx, c in enumerate(r):
-            dp[r_idx + 1][c_idx + 1] = c
+            # 1행 부터 dp실행
+            if r_idx == 0:
+                continue
+            
+            # 0열은 우측 대각선 상단 값만 / 마지막 열은 좌측 대각선 상단 값만 / 그외는 둘 중 더 큰값이 옴
+            if c_idx == 0:
+                triangle[r_idx][c_idx] += triangle[r_idx-1][c_idx]
+            elif c_idx == r_length - 1:
+                triangle[r_idx][c_idx] += triangle[r_idx-1][c_idx-1]
+            else:
+                triangle[r_idx][c_idx] += max(triangle[r_idx-1][c_idx], triangle[r_idx-1][c_idx-1])
     
-    for a in range(1, row):
-        for b in range(1, col):
-            dp[a][b] += max(dp[a-1][b], dp[a-1][b-1])
     
-    return max(dp[-1])
+    return max(triangle[-1])
